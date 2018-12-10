0635e4c2c0feee2362047f50de61bd08f72b2a2f
neal.okelly100@gmail.com <Cra56056!>
2018-12-10 14:14:13 +0000

    New database size screen.

---
+++ assets/charts/database-size.js
@@ -0,0 +1,35 @@
+      google.charts.load('current', {'packages':['corechart']});
+	  google.charts.setOnLoadCallback(drawDeferredUploadChart);
+
+
+	
+		
+   function drawDeferredUploadChart() {
+      var range = 'M2:U16';
+
+      var query = new google.visualization.Query(
+          'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
+      query.send(handleDeferredUploadQueryResponse);
+ 
+    }
+
+    function handleDeferredUploadQueryResponse(response) {
+      if (response.isError()) {
+        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+      }
+
+		var options = {
+            title: 'Size of seleced table spaces (kB)',
+			colors: ['#d53880', '#006435', '#b10e1e', '#ffbf47', '#912b88', '#f47738', '#28a197', '#b58840'],
+			isStacked: 'true',
+			vAxis: {format: 'long'
+        }
+         };
+	
+		
+var data = response.getDataTable();
+      var chart = new google.visualization.AreaChart(document.getElementById('database-size-chart'));
+        chart.draw(data, options);
+
+    }
