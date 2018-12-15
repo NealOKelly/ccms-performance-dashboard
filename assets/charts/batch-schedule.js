01073b47b8e3a4cff40668871132c30c37aba48d
neal.okelly100@gmail.com <Cra56056!>
2018-12-15 19:44:59 +0000

    Source code formatting.

--- assets/charts/batch-schedule.js
+++ assets/charts/batch-schedule.js
@@ -1,133 +1,155 @@
-      google.charts.load('current', {'packages':['bar']});
+      google.charts.load('current', {
+      	'packages': ['bar']
+      });
       google.charts.setOnLoadCallback(drawSet1Chart);
-	  google.charts.setOnLoadCallback(drawSet2Chart);
-	  google.charts.setOnLoadCallback(drawSet3Chart);
-	  google.charts.setOnLoadCallback(drawPSChart);
+      google.charts.setOnLoadCallback(drawSet2Chart);
+      google.charts.setOnLoadCallback(drawSet3Chart);
+      google.charts.setOnLoadCallback(drawPSChart);
 
 
-	
-		
-   function drawSet1Chart() {
-      var range = 'A2:B16';
 
-      var query = new google.visualization.Query(
-          'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
-      query.send(handleSet1QueryResponse);
- 
-    }
 
-    function handleSet1QueryResponse(response) {
-      if (response.isError()) {
-        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
+      function drawSet1Chart() {
+      	var range = 'A2:B16';
+
+      	var query = new google.visualization.Query(
+      		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
+      	query.send(handleSet1QueryResponse);
+
       }
 
-		var options = {
-            title: 'Continuous Background Set 1',
-            subtitle: 'Average runtime for the last 14 days',
-			colors: ['#d53880'],
-			legend: {position: 'none'},
-			vAxis: {title: 'minutes',
-        },
-          };
-	
-		
-      var data = response.getDataTable();
-      var chart = new google.charts.Bar(document.getElementById('continuous-background-set-1'));
-      chart.draw(data, { height: 400 });
-	chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
+      function handleSet1QueryResponse(response) {
+      	if (response.isError()) {
+      		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      		return;
+      	}
 
-		
-   function drawSet2Chart() {
-      var range = 'C2:D16';
+      	var options = {
+      		title: 'Continuous Background Set 1',
+      		subtitle: 'Average runtime for the last 14 days',
+      		colors: ['#d53880'],
+      		legend: {
+      			position: 'none'
+      		},
+      		vAxis: {
+      			title: 'minutes',
+      		},
+      	};
 
-      var query = new google.visualization.Query(
-          'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
-      query.send(handleSet2QueryResponse);
- 
-    }
 
-    function handleSet2QueryResponse(response) {
-      if (response.isError()) {
-        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
+      	var data = response.getDataTable();
+      	var chart = new google.charts.Bar(document.getElementById('continuous-background-set-1'));
+      	chart.draw(data, {
+      		height: 400
+      	});
+      	chart.draw(data, google.charts.Bar.convertOptions(options));
       }
 
-		var options = {
-            title: 'Continuous Background Set 2',
-            subtitle: 'Average runtime for the last 14 days',
-			colors: ['#d53880'],
-			legend: {position: 'none'},
-			vAxis: {title: 'minutes',
-        },
-          };
-	
-		
-      var data = response.getDataTable();
-      var chart = new google.charts.Bar(document.getElementById('continuous-background-set-2'));
-      chart.draw(data, { height: 400 });
-	chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
 
-   function drawSet3Chart() {
-      var range = 'E2:F16';
+      function drawSet2Chart() {
+      	var range = 'C2:D16';
 
-      var query = new google.visualization.Query(
-          'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
-      query.send(handleSet3QueryResponse);
- 
-    }
+      	var query = new google.visualization.Query(
+      		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
+      	query.send(handleSet2QueryResponse);
 
-    function handleSet3QueryResponse(response) {
-      if (response.isError()) {
-        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
       }
 
-		var options = {
-            title: 'Continuous Background Set 3',
-            subtitle: 'Average runtime for the last 14 days',
-			colors: ['#d53880'],
-			legend: {position: 'none'},
-			vAxis: {title: 'minutes',
-        },
-          };
-	
-		
-      var data = response.getDataTable();
-      var chart = new google.charts.Bar(document.getElementById('continuous-background-set-3'));
-      chart.draw(data, { height: 400 });
-	chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
+      function handleSet2QueryResponse(response) {
+      	if (response.isError()) {
+      		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      		return;
+      	}
 
-   function drawPSChart() {
-      var range = 'G2:H16';
+      	var options = {
+      		title: 'Continuous Background Set 2',
+      		subtitle: 'Average runtime for the last 14 days',
+      		colors: ['#d53880'],
+      		legend: {
+      			position: 'none'
+      		},
+      		vAxis: {
+      			title: 'minutes',
+      		},
+      	};
 
-      var query = new google.visualization.Query(
-          'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
-      query.send(handlePSQueryResponse);
- 
-    }
 
-    function handlePSQueryResponse(response) {
-      if (response.isError()) {
-        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
+      	var data = response.getDataTable();
+      	var chart = new google.charts.Bar(document.getElementById('continuous-background-set-2'));
+      	chart.draw(data, {
+      		height: 400
+      	});
+      	chart.draw(data, google.charts.Bar.convertOptions(options));
       }
 
-		var options = {
-            title: 'Provider Synchronisation',
-            subtitle: 'Average runtime for the last 14 days',
-			colors: ['#d53880'],
-			legend: {position: 'none'},
-			vAxis: {title: 'minutes',
-        },
-          };
-	
-		
-      var data = response.getDataTable();
-      var chart = new google.charts.Bar(document.getElementById('provider-synchronisation'));
-      chart.draw(data, { height: 400 });
-	chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
+      function drawSet3Chart() {
+      	var range = 'E2:F16';
+
+      	var query = new google.visualization.Query(
+      		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
+      	query.send(handleSet3QueryResponse);
+
+      }
+
+      function handleSet3QueryResponse(response) {
+      	if (response.isError()) {
+      		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      		return;
+      	}
+
+      	var options = {
+      		title: 'Continuous Background Set 3',
+      		subtitle: 'Average runtime for the last 14 days',
+      		colors: ['#d53880'],
+      		legend: {
+      			position: 'none'
+      		},
+      		vAxis: {
+      			title: 'minutes',
+      		},
+      	};
+
+
+      	var data = response.getDataTable();
+      	var chart = new google.charts.Bar(document.getElementById('continuous-background-set-3'));
+      	chart.draw(data, {
+      		height: 400
+      	});
+      	chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+
+      function drawPSChart() {
+      	var range = 'G2:H16';
+
+      	var query = new google.visualization.Query(
+      		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
+      	query.send(handlePSQueryResponse);
+
+      }
+
+      function handlePSQueryResponse(response) {
+      	if (response.isError()) {
+      		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+      		return;
+      	}
+
+      	var options = {
+      		title: 'Provider Synchronisation',
+      		subtitle: 'Average runtime for the last 14 days',
+      		colors: ['#d53880'],
+      		legend: {
+      			position: 'none'
+      		},
+      		vAxis: {
+      			title: 'minutes',
+      		},
+      	};
+
+
+      	var data = response.getDataTable();
+      	var chart = new google.charts.Bar(document.getElementById('provider-synchronisation'));
+      	chart.draw(data, {
+      		height: 400
+      	});
+      	chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
--- assets/charts/network-latency.js
+++ assets/charts/network-latency.js
@@ -1,133 +1,147 @@
-      google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawComparisonChart);
-	  google.charts.setOnLoadCallback(drawVarianceVDIChart);
-	  google.charts.setOnLoadCallback(drawVarianceInternetChart);
-	  google.charts.setOnLoadCallback(drawVarianceDom1Chart);
-
-	
-		
-   function drawComparisonChart() {
-      var range = 'A2:D16';
-
-      var query = new google.visualization.Query(
-          'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data-Latency&headers=1&tq&range=' + range);
-      query.send(handleComparisonQueryResponse);
- 
-    }
-
-    function handleComparisonQueryResponse(response) {
-      if (response.isError()) {
-        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
-      }
-
-		var options = {
-            title: 'Round Trip Time between the client and the forms server for VDI, internet and Dom1 users',
-            colors: ['#d53880', '#28a197', '#005ea5'],
-			vAxis: {title: 'milliseconds',
-        }
-			};
-	
-		
-      var data = response.getDataTable();
-      var chart = new google.charts.Bar(document.getElementById('comparison'));
-      chart.draw(data, { height: 400 });
-	chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
-
-		
-				// variance chart VDI
-		   function drawVarianceVDIChart() {
-      var range = 'F2:I16';
-
-      var query = new google.visualization.Query(
-          'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data-Latency&headers=1&tq&range=' + range);
-      query.send(handleVarianceVDIQueryResponse);
- 
-    }
-
-		
-
-    function handleVarianceVDIQueryResponse(response) {
-      if (response.isError()) {
-        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
-      }
-
-		var options = {
-            title: 'VDI: minimum, average and maximum Round Trip Time',
-			colors: ['#006435', '#ffbf47', '#b10e1e'],
-			vAxis: {title: 'milliseconds',
-        }
-          };
-
-      var data = response.getDataTable();
-      var chart = new google.charts.Bar(document.getElementById('variance_vdi'));
-      chart.draw(data, { height: 400 });
-	chart.draw(data, google.charts.Bar.convertOptions(options));
-    }
-		
-					// variance chart internet
-		   function drawVarianceInternetChart() {
-			   
-      var range = 'K2:N16';
-
-      var query = new google.visualization.Query(
-          'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data-Latency&headers=1&tq&range=' + range);
-      query.send(handleVarianceInternetQueryResponse);
- 
-    }
-
-		
-
-    function handleVarianceInternetQueryResponse(response) {
-      if (response.isError()) {
-        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
-      }
-
-		var options = {
-            title: 'Internet: minimum, average and maximum Round Trip Time',
-            colors: ['#006435', '#ffbf47', '#b10e1e'],
-			vAxis: {title: 'milliseconds',
-        }
-          };
-
-      var data = response.getDataTable();
-      var chart = new google.charts.Bar(document.getElementById('variance_internet'));
-      chart.draw(data, { height: 400 });
-	chart.draw(data, google.charts.Bar.convertOptions(options));
-    }	
-	
-						// variance dom1 internet
-		   function drawVarianceDom1Chart() {
-			   
-      var range = 'P2:S16';
-
-      var query = new google.visualization.Query(
-          'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data-Latency&headers=1&tq&range=' + range);
-      query.send(handleVarianceDom1QueryResponse);
- 
-    }
-
-		
-
-    function handleVarianceDom1QueryResponse(response) {
-      if (response.isError()) {
-        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-        return;
-      }
-
-		var options = {
-            title: 'Dom1: minimum, average and maximum Round Trip Time',
-            colors: ['#006435', '#ffbf47', '#b10e1e'],
-			vAxis: {title: 'milliseconds',
-        }
-          };
+  google.charts.load('current', {
+  	'packages': ['bar']
+  });
+  google.charts.setOnLoadCallback(drawComparisonChart);
+  google.charts.setOnLoadCallback(drawVarianceVDIChart);
+  google.charts.setOnLoadCallback(drawVarianceInternetChart);
+  google.charts.setOnLoadCallback(drawVarianceDom1Chart);
 
 
-      var data = response.getDataTable();
-      var chart = new google.charts.Bar(document.getElementById('variance_dom1'));
-      chart.draw(data, { height: 400 });
-	chart.draw(data, google.charts.Bar.convertOptions(options));
-    }	
+
+  function drawComparisonChart() {
+  	var range = 'A2:D16';
+
+  	var query = new google.visualization.Query(
+  		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data-Latency&headers=1&tq&range=' + range);
+  	query.send(handleComparisonQueryResponse);
+
+  }
+
+  function handleComparisonQueryResponse(response) {
+  	if (response.isError()) {
+  		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+  		return;
+  	}
+
+  	var options = {
+  		title: 'Round Trip Time between the client and the forms server for VDI, internet and Dom1 users',
+  		colors: ['#d53880', '#28a197', '#005ea5'],
+  		vAxis: {
+  			title: 'milliseconds',
+  		}
+  	};
+
+
+  	var data = response.getDataTable();
+  	var chart = new google.charts.Bar(document.getElementById('comparison'));
+  	chart.draw(data, {
+  		height: 400
+  	});
+  	chart.draw(data, google.charts.Bar.convertOptions(options));
+  }
+
+
+  // variance chart VDI
+  function drawVarianceVDIChart() {
+  	var range = 'F2:I16';
+
+  	var query = new google.visualization.Query(
+  		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data-Latency&headers=1&tq&range=' + range);
+  	query.send(handleVarianceVDIQueryResponse);
+
+  }
+
+
+
+  function handleVarianceVDIQueryResponse(response) {
+  	if (response.isError()) {
+  		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+  		return;
+  	}
+
+  	var options = {
+  		title: 'VDI: minimum, average and maximum Round Trip Time',
+  		colors: ['#006435', '#ffbf47', '#b10e1e'],
+  		vAxis: {
+  			title: 'milliseconds',
+  		}
+  	};
+
+  	var data = response.getDataTable();
+  	var chart = new google.charts.Bar(document.getElementById('variance_vdi'));
+  	chart.draw(data, {
+  		height: 400
+  	});
+  	chart.draw(data, google.charts.Bar.convertOptions(options));
+  }
+
+  // variance chart internet
+  function drawVarianceInternetChart() {
+
+  	var range = 'K2:N16';
+
+  	var query = new google.visualization.Query(
+  		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data-Latency&headers=1&tq&range=' + range);
+  	query.send(handleVarianceInternetQueryResponse);
+
+  }
+
+
+
+  function handleVarianceInternetQueryResponse(response) {
+  	if (response.isError()) {
+  		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+  		return;
+  	}
+
+  	var options = {
+  		title: 'Internet: minimum, average and maximum Round Trip Time',
+  		colors: ['#006435', '#ffbf47', '#b10e1e'],
+  		vAxis: {
+  			title: 'milliseconds',
+  		}
+  	};
+
+  	var data = response.getDataTable();
+  	var chart = new google.charts.Bar(document.getElementById('variance_internet'));
+  	chart.draw(data, {
+  		height: 400
+  	});
+  	chart.draw(data, google.charts.Bar.convertOptions(options));
+  }
+
+  // variance dom1 internet
+  function drawVarianceDom1Chart() {
+
+  	var range = 'P2:S16';
+
+  	var query = new google.visualization.Query(
+  		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data-Latency&headers=1&tq&range=' + range);
+  	query.send(handleVarianceDom1QueryResponse);
+
+  }
+
+
+
+  function handleVarianceDom1QueryResponse(response) {
+  	if (response.isError()) {
+  		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+  		return;
+  	}
+
+  	var options = {
+  		title: 'Dom1: minimum, average and maximum Round Trip Time',
+  		colors: ['#006435', '#ffbf47', '#b10e1e'],
+  		vAxis: {
+  			title: 'milliseconds',
+  		}
+  	};
+
+
+  	var data = response.getDataTable();
+  	var chart = new google.charts.Bar(document.getElementById('variance_dom1'));
+  	chart.draw(data, {
+  		height: 400
+  	});
+  	chart.draw(data, google.charts.Bar.convertOptions(options));
+  }
