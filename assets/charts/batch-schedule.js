d4655dd1a21822ad7e088fad65a458dd69a9ed5f
neal.okelly100@gmail.com <Cra56056!>
2018-12-17 10:21:37 +0000

    Updated reporting period for 14 to 28 days.

--- assets/charts/batch-schedule.js
+++ assets/charts/batch-schedule.js
@@ -10,7 +10,7 @@
 
 
       function drawSet1Chart() {
-      	var range = 'A2:B16';
+      	var range = 'A2:B30';
 
       	var query = new google.visualization.Query(
       		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
@@ -26,7 +26,7 @@
 
       	var options = {
       		title: 'Continuous Background Set 1',
-      		subtitle: 'Average runtime for the last 14 days',
+      		subtitle: 'Average runtime for the last 28 days',
       		colors: ['#d53880'],
       		legend: {
       			position: 'none'
@@ -47,7 +47,7 @@
 
 
       function drawSet2Chart() {
-      	var range = 'C2:D16';
+      	var range = 'C2:D30';
 
       	var query = new google.visualization.Query(
       		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
@@ -63,7 +63,7 @@
 
       	var options = {
       		title: 'Continuous Background Set 2',
-      		subtitle: 'Average runtime for the last 14 days',
+      		subtitle: 'Average runtime for the last 28 days',
       		colors: ['#d53880'],
       		legend: {
       			position: 'none'
@@ -83,7 +83,7 @@
       }
 
       function drawSet3Chart() {
-      	var range = 'E2:F16';
+      	var range = 'E2:F30';
 
       	var query = new google.visualization.Query(
       		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
@@ -99,7 +99,7 @@
 
       	var options = {
       		title: 'Continuous Background Set 3',
-      		subtitle: 'Average runtime for the last 14 days',
+      		subtitle: 'Average runtime for the last 28 days',
       		colors: ['#d53880'],
       		legend: {
       			position: 'none'
@@ -119,7 +119,7 @@
       }
 
       function drawPSChart() {
-      	var range = 'G2:H16';
+      	var range = 'G2:H30';
 
       	var query = new google.visualization.Query(
       		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
@@ -135,7 +135,7 @@
 
       	var options = {
       		title: 'Provider Synchronisation',
-      		subtitle: 'Average runtime for the last 14 days',
+      		subtitle: 'Average runtime for the last 28 days',
       		colors: ['#d53880'],
       		legend: {
       			position: 'none'
