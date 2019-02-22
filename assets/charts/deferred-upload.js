70cdcc7258bcd9a696a8d2b9cdb4f09da9936f12
neal.okelly100@gmail.com <Cra56056!>
2019-02-22 21:15:52 +0000

    Changed the width to use more of the page.

--- assets/charts/deferred-upload.js
+++ assets/charts/deferred-upload.js
@@ -1,14 +1,14 @@
 google.charts.load('current', {
 	'packages': ['corechart']
-});
+}).then(drawDeferredUploadChart);
 google.charts.setOnLoadCallback(drawDeferredUploadChart);
 
 
 function drawDeferredUploadChart() {
-	var range = 'J2:K';
+	var range = 'A2:B';
 
 	var query = new google.visualization.Query(
-		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Chart-Data&headers=1&tq&range=' + range);
+		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=Deferred-Upload&headers=1&tq&range=' + range);
 	query.send(handleDeferredUploadQueryResponse);
 
 }
@@ -27,12 +27,15 @@ function handleDeferredUploadQueryResponse(response) {
 		},
 		vAxis: {
 			ticks: [0, 50000, 100000, 150000, 200000, 250000, 300000, 350000, 400000]
-		}
+		},
+		chartArea: {
+			width: 500
+					}
 	};
 
 
 	var data = response.getDataTable();
 	var chart = new google.visualization.AreaChart(document.getElementById('deferred-upload-chart'));
 	chart.draw(data, options);
-
+window.addEventListener('resize', drawDeferredUploadChart, false);
 }
