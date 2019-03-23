bc1707a4fc540a2f5c3677c2c6b6419942fe4939
neal.okelly100@gmail.com <Cra56056!>
2019-03-23 15:39:55 +0000

    Changed App Review chart to a line chart.

--- assets/charts/database-performance-brm-app-review.js
+++ assets/charts/database-performance-brm-app-review.js
@@ -1,44 +1,42 @@
-      google.charts.load('current', {
-      	'packages': ['bar']
-      });
-      google.charts.setOnLoadCallback(drawDBPerformanceBRMChart);
+google.charts.load('current', {
+	'packages': ['corechart']
+}).then(drawDBPerformanceBRMChartAppReview);
+google.charts.setOnLoadCallback(drawDBPerformanceBRMChartAppReview);
 
 
-      function drawDBPerformanceBRMChart() {
-      	var range = 'A:B';
+function drawDBPerformanceBRMChartAppReview() {
+	var range = 'A2:B';
 
       	var query = new google.visualization.Query(
       		'https://docs.google.com/spreadsheets/d/1gUcd_R_vnJDtSNlkrSGsvcJe-bKd8lDqJALeDAKEYpA/gviz/tq?sheet=BRM-Performance&headers=1&tq&range=' + range);
-      	query.send(handleDBPerformanceBRMQueryResponse);
+      	query.send(handleDBPerformanceBRMAppReviewQueryResponse);
 
       }
 
-      function handleDBPerformanceBRMQueryResponse(response) {
-      	if (response.isError()) {
-      		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-      		return;
-      	}
+function handleDBPerformanceBRMAppReviewQueryResponse(response) {
+	if (response.isError()) {
+		alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+		return;
+	}
 
-      	var options = {
-      		title: 'Business Rule Monitor',
-      		subtitle: 'Average runtime of App Review',
-      		colors: ['#d53880'],
-      		legend: {
-      			position: 'none'
-      		},
-      		vAxis: {
-      			title: 'Minutes',
-      		},
-      	};
-
-
-      	var data = response.getDataTable();
-      	var chart = new google.charts.Bar(document.getElementById('database-performance-brm-chart'));
-      	chart.draw(data, {
-      		height: 400
-      	});
-      	chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
+	var options = {
+   		title: 'Average runtime of App Review',
+		colors: ['#d53880'],
+		legend: {
+			position: 'none'
+		},
+		vAxis: {
+		title: 'Minutes',
+   		},
+		chartArea: {
+			width: 500
+					}
+	};
 
 
+	var data = response.getDataTable();
+	var chart = new google.visualization.AreaChart(document.getElementById('database-performance-brm-chart-app-review'));
+	chart.draw(data, options);
+window.addEventListener('resize', handleDBPerformanceBRMAppReviewQueryResponse, false);
+}
       
